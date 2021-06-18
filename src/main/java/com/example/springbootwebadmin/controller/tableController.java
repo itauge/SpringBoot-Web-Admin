325ee280273686b2d3e92fb0a4877c9345cd8605
itauge <itauge@gmail.com>
2021-06-18 18:49:37 +0800

    another version

--- src/main/java/com/example/springbootwebadmin/controller/tableController.java
+++ src/main/java/com/example/springbootwebadmin/controller/tableController.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -52,6 +54,17 @@ public class tableController {
 
 
         return "table/dynamic_table";
+    }
+
+    @GetMapping("/user/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id,
+                             @RequestParam(value = "pg",defaultValue = "1") Integer pg,
+                             RedirectAttributes ra){
+
+        userService.removeById(id);
+        ra.addAttribute("pg",pg);
+
+        return "redirect:/dynamic_table";
     }
 
     @GetMapping("/responsive_table")
